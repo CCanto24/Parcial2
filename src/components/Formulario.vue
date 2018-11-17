0ae1814dc5fd59828cabd955f580db800f23a7b3
Carlos Alberto Canto <chambeto24@hotmail.com>
2018-11-17 00:58:04 -0500

    cree todos los archivos, finalizo el login y se avanzo con el formulario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+    <center>
+       <h1 align="center"> Ingrese los datos de Usuario </h1>>
+     <el-col :span="12">
+      <div class="grid-content">
+        <el-input placeholder="Nombre" v-model="Nombre" ></el-input>
+        <br>
+        <br>
+        <el-input placeholder="Apellido" v-model="Apellido" ></el-input>
+        <br>
+        <br>
+        <div> 
+            <el-select placeholder="Provincia" v-model="Ciudad">
+                <el-option
+                v-for="item in provincias"
+                :key="item.provincia"
+                :label="item.label"
+                :value="item.value">
+                </el-option>
+            </el-select>
+        </div>
+        <br>
+        <p> Sexo: </p>
+        <el-radio v-model="Sexo" label="1">Masculino</el-radio>
+        <el-radio v-model="Sexo" label="2">Femenino</el-radio>
+        <br>
+        <p> Estado Civil: </p>
+        <el-radio v-model="EstCiv" Label="1">Soltero/a</el-radio>
+        <el-radio v-model="EstCiv" Label="2">Casado/a</el-radio>
+        <el-radio v-model="EstCiv" Label="3">Divorciado/a</el-radio>
+        <el-radio v-model="EstCiv" Label="4">Viudo/a</el-radio>
+        <br>
+        <p> Estado: </p>
+        <el-radio v-model="Estado" Label="1">Activo</el-radio>
+        <el-radio v-model="Estado" Label="2">Inactivo</el-radio>
+        <br>
+        <br>
+        <el-input placeholder="Dirección" v-model="Direccion"></el-input>
+        <br>
+        <br>
+      </div>
+      </el-col>
+      </center>
+</div>
+
+</template>
+<script>
+export default {
+    name:"formulario",
+    data: function() {
+
+    }
+
+}
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,55 @@
+ <template>
+    <div>
+        <center v-if="!check">
+            <h2> Inicio de Sesión </h2>
+            <p>Ingrese su usuario y contraseña </p>
+            <el-input size="medium" placeholder="Usuario" v-model="User"></el-input>
+            <br>
+            <br>
+            <el-input size="medium" placeholder="Contraseña" v-model="Pass" type="password"></el-input>
+            <br>
+            <br>
+            <el-button type="primary" @click="validar" >Ingresar </el-button>
+
+        </center>
+        <center v-if="check">
+            <formulario/> 
+
+        </center>
+
+    </div>
+</template>
+<script>
+import formulario from './Formulario.vue'
+
+export default {
+    name: "login",
+    components:{
+        formulario
+    },
+    data: function(){
+        return {
+            User:'',
+            Pass:'',
+            check: false,
+        }
+    },
+       methods:
+       {
+            validar: function()
+            {
+            if (this.User === 'admin' && this.Pass === 'admin')
+                {
+                 this.check = true
+                }
+            else 
+                {
+                alert('Usuario o Contraseña no valido')
+                document.getElementById("nombre").value="";
+                document.getElementById("clave").value="";
+                document.getElementById("nombre").focus();
+                }
+            }
+       }
+}
+</script>
